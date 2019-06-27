84735f09c3c1ebca2306f29aa6808f2ddff9fdb6
Levi Rosenberg <levirosenberg97@gmail.com>
2019-06-27 15:07:56 -0700

    added in the basic Left and Right movement

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    Vector3 currentPosition;
+
+    private void Start()
+    {
+        currentPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            currentPosition.x -= Time.deltaTime * speed;
+            transform.position = currentPosition;          
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            currentPosition.x += Time.deltaTime * speed;
+            transform.position = currentPosition;
+        }
+    }
+
+}
