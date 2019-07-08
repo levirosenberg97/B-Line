54092bad2844de8ae68c74c4777b5754fb8502b6
Shea McAuley <thespookter5123@gmail.com>
2019-07-08 14:38:28 -0700

    MASTER

---
+++ Assets/playerInteract.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInteract : MonoBehaviour {
+
+
+    public void ScreenMouseRay()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5f;
+
+        Vector2 v = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Collider2D[] col = Physics2D.OverlapPointAll(v);
+
+        if (col.Length > 0)
+        {
+            foreach (Collider2D c in col)
+            {
+                //Debug.Log("Collided with: " + c.collider2D.gameObject.name);
+                targetPos = c.collider2D.gameObject.transform.position;
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
