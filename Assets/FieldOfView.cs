fc09df96d81664b8e9ef8873e631d83d4cb3b913
Shea McAuley <thespookter5123@gmail.com>
2019-07-09 13:01:18 -0700

    Render farm?

---
+++ Assets/FieldOfView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+    public Vector3 gizmoRotation;
+
+    public Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if(!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
---
+++ Assets/cameraControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour {
+
+    public Transform pos;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = pos.position + new Vector3(0,0,-1);
+	}
+}
