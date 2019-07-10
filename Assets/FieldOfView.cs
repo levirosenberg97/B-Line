1303e040f0f8e7ca30aea842c18008ccd1978544
Shea McAuley <thespookter5123@gmail.com>
2019-07-09 18:00:16 -0700

    More money

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -22,7 +22,12 @@ public class FieldOfView : MonoBehaviour {
             Vector3 dirToTarget = (target.position - transform.position).normalized;
             if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
             {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
 
+                if(!Physics.Raycast(transform.position, dirToTarget,dstToTarget,obstacleMask))
+                {
+
+                }
             }
         }
     }
