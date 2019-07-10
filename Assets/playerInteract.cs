8b8760405bf44fd8c9a7cc3ada8ff4e98daf3906
Levi Rosenberg <levirosenberg97@gmail.com>
2019-07-09 17:52:28 -0700

    Added an Outline Shader ad began work on the Player Interaction Script

--- Assets/playerInteract.cs
+++ Assets/playerInteract.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerInteract : MonoBehaviour {
+public class playerInteract : MonoBehaviour
+{
+    public Color defaultOutline, newOutline;
+    public float speed = 1, offset;
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock propBlock;
+
+    private void Awake()
+    {
+        propBlock = new MaterialPropertyBlock();
+        _renderer = GetComponent<Renderer>();
+
+        //propBlock.SetColor("_Color", Color.black);
+    }
+
+    private void Update()
+    {
+            _renderer.GetPropertyBlock(propBlock);
+
+            propBlock.SetColor("_Color", Color.Lerp(defaultOutline, newOutline, (Mathf.Sin(Time.time * speed + offset) + 1) / 2f));
+
+            _renderer.SetPropertyBlock(propBlock);
+        
+    }
 
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("Hit");
+           
+        }
+    }
 }
