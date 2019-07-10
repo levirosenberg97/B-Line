daab0e74f2e9aa51e082064592121c8b5e518c7b
Shea McAuley <thespookter5123@gmail.com>
2019-07-09 17:59:47 -0700

    Shea worked in master again lol

--- Assets/FieldOfView.cs
+++ Assets/FieldOfView.cs
@@ -9,11 +9,29 @@ public class FieldOfView : MonoBehaviour {
     public float viewAngle;
     public Vector3 gizmoRotation;
 
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    void  FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for(int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+
+            }
+        }
+    }
+
     public Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         if(!angleIsGlobal)
         {
-            angleInDegrees += transform.eulerAngles.y;
+            angleInDegrees -= transform.eulerAngles.z;
         }
 
         return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
